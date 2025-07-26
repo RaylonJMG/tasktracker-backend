f90decb64cd1d7ac403b3f5ea4fedd7166c79c4b
RaylonJMG <ryj2213@gmail.com>
2025-07-26 00:12:42 -0500

    Fix import statements in user controller and route files

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,4 +1,4 @@
-import { pool } from "../db/connection";
+import { pool } from "../db/connection.js";
 import jwt from "jsonwebtoken";
 
 export const postUser = async (req, res) => {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { auth, postUser } from "../controllers/user_controller";
+import { auth, postUser } from "../controllers/user_controller.js";
 
 export const user = express.Router();
 
