e5c75ab3c556dfb3175f9246a00c3972e6a21ccf
RaylonJMG <ryj2213@gmail.com>
2025-07-25 21:18:23 -0500

    Implement HTTPS support for production environment

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { tasks } from "./routes/tasksRoute.js";
 import { employee } from "./routes/employeeRoute.js";
+import fs from "fs";
+import https from "https";
 
 dotenv.config();
 const app = express();
+const environment = process.env.NODE_ENV;
+let port = 4000;
+
+if (environment === "production") {
+	port = 443;
+}
 
 app.use(express.json());
 app.use(cors());
@@ -13,7 +21,20 @@ app.use("/api", employee);
 app.use("/api", tasks);
 app.use("/api", user);
 
-const port = 4000;
-app.listen(port, () => {
-	console.log(`server listening on port ${port}`);
-});
+if (environment === "production") {
+	const options = {
+		key: fs.readFileSync(
+			"/etc/letsencrypt/live/raylon-backend.codex-p4-2025.click/privkey.pem"
+		),
+		cert: fs.readFileSync(
+			"/etc/letsencrypt/live/raylon-backend.codex-p4-2025.click/fullchain.pem"
+		),
+	};
+	https.createServer(options, app).listen(port, () => {
+		console.log("HTTPS server is running on port 443");
+	});
+} else {
+	app.listen(port, () => {
+		console.log(`server listening on port ${port}`);
+	});
+}
