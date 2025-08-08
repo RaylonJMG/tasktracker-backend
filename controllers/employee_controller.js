8ae2767723643ffa62389a7b4dacc78bde3ca9db
RaylonJMG <ryj2213@gmail.com>
2025-08-08 18:37:05 -0500

    fix:sort

--- controllers/employee_controller.js
+++ controllers/employee_controller.js
@@ -10,7 +10,15 @@ export const postEmployee = async (req, res) => {
 };
 //READ EMPLOYEE
 export const getAllEmployees = async (req, res) => {
-	const sql = `select * from task_tracker.employee`;
+	const body = req.body;
+	const parameters = [body.employee_id, body.name, body.department, body.role];
+	const sql = `select 
+					employee_id,
+					name,
+					department,
+					role
+				from task_tracker.employee
+				order by employee_id asc`;
 	const result = await pool.query(sql);
 	return res.json(result.rows);
 };
@@ -36,7 +44,7 @@ export const putEmployee = async (req, res) => {
 						role = $3
 				where employee_id = $4`;
 	const body = req.body;
-	const employee_id = req.params.employee_id;
+	const { employee_id } = req.params;
 	const parameters = [body.name, body.department, body.role, employee_id];
 	const result = await pool.query(sql, parameters);
 	return res.json({ message: "Success: Employee Updated" });
@@ -44,7 +52,7 @@ export const putEmployee = async (req, res) => {
 //DELETE EMPLOYEE
 export const deleteEmployee = async (req, res) => {
 	const sql = `delete from task_tracker.employee where employee_id = $1`;
-	const employee_id = req.params.employee_id;
+	const { employee_id } = req.params;
 	const parameters = [employee_id];
 	const result = await pool.query(sql, parameters);
 	return res.json({ message: "Success: Employee removed" });
